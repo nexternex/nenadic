fe29c9daeb6a4aca50eb273834c49ebacef972b9
nexnexter <nexnexter@gmail.com>
2015-10-06 15:35:24 +0200

    1

--- app.js
+++ app.js
@@ -3,24 +3,21 @@ var express = require('express');
     routes = require('./routes');
 var app = module.exports=express();
 
-
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-app.use(express.bodyParser());
-app.use(express.methodOverride());
 // views is directory for all template files
+app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.static(__dirname + '/public'));
   app.use(app.router);
+});
+
 //GET ROUTES
 app.get('/', routes.index);
 app.get('/partials/:name', routes.partials);
-
 app.get('*', routes.index)
 
 app.listen(app.get('port'), function() {
