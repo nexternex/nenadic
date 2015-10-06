17840bc27ccdd4d3979f7099f5d68fbff5eec5f5
nexnexter <nexnexter@gmail.com>
2015-10-06 13:19:49 +0200

    IN

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
 
-  res.sendfile(__dirname + '/public/index.html');
-});
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
