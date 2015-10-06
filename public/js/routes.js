b168cd9d4f808ba54aee9971b45d0e16a2fd8ca9
nexnexter <nexnexter@gmail.com>
2015-10-06 14:05:30 +0200

    IN

--- public/js/routes.js
+++ public/js/routes.js
@@ -5,11 +5,11 @@ erpagWeather.config(function ($routeProvider){
 $routeProvider
 
     .when('/',{
-    templateUrl:'/pages/Home.ejs',
+    templateUrl:'pages/Home.ejs',
     controller:'homeController'
     })
     .when('/Home',{
-    templateUrl:'/pages/Home.ejs',
+    templateUrl:'pages/Home.ejs',
     controller:'homeController'
     })
 
