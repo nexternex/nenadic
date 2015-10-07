e626ecb6721c5cc27062f03639415bc7d197ce3d
nexnexter <nexnexter@gmail.com>
2015-10-07 15:36:17 +0200

    u

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,18 @@
 //MODULE
-var erpagWeather=angular.module('erpagWeather',['ngRoute','ngResource','auth0','angular-storage','angular-jwt']);
+var erpagWeather=angular.module('erpagWeather',['ngRoute','ngResource']);
 
-
+// app.js
+angular.module('erpagWeather', ['auth0', 'angular-storage', 'angular-jwt'])
+.config(function (authProvider) {
+  authProvider.init({
+    domain: 'myday.eu.auth0.com',
+    clientID: 'oFP2Wct8YTQvmWjmKAk4YgGmfLVZwJsl'
+  });
+})
+.run(function(auth) {
+  // This hooks al auth events to check everything as soon as the app starts
+  auth.hookEvents();
+});
 
 
 erpagWeather.config(function (authProvider) {
