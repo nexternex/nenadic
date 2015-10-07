6fb1c8d0a2f853785feed31661c0acbe171c5f38
nexnexter <nexnexter@gmail.com>
2015-10-07 14:47:07 +0200

    in

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,12 @@
 //MODULE
-var erpagWeather=angular.module('erpagWeather',['ngRoute','ngResource']);
+var erpagWeather=angular.module('erpagWeather',['ngRoute','ngResource','auth0','angular-storage', 'angular-jwt'])
+.config(function (authProvider) {
+  authProvider.init({
+    domain: 'myday.eu.auth0.com',
+    clientID: 'oFP2Wct8YTQvmWjmKAk4YgGmfLVZwJsl'
+  });
+})
+.run(function(auth) {
+  // This hooks al auth events to check everything as soon as the app starts
+  auth.hookEvents();
+});
--- public/js/routes.js
+++ public/js/routes.js
@@ -16,7 +16,24 @@ $routeProvider
     .when('/Forecast', {
     templateUrl:'../pages/forecast.htm',
     controller:'forecastController'
-    })
-
-
+    });
+    
+    $routeProvider.when('/login', {
+    templateUrl: 'login.tpl.html',
+    controller: 'LoginCtrl'
+  });
+    // Logged in route
+  $routeProvider.when('/user-info', {
+    templateUrl: 'userInfo.tpl.html',
+    controller: 'UserInfoCtrl',
+    requiresLogin: true
+  });
+      authProvider.init({
+    domain: 'myday.eu.auth0.com',
+    clientID: 'oFP2Wct8YTQvmWjmKAk4YgGmfLVZwJsl',
+    callbackURL: location.href,
+    // Here include the URL to redirect to if the user tries to access a resource when not authenticated.
+    loginUrl: '/login'
+  });
 });
+
