66fdce28fc48666803dd1b75f480c6531869901b
nexternex <nexnexter@gmail.com>
2015-10-08 00:15:55 +0200

    s

--- public/js/app.js
+++ public/js/app.js
@@ -2,46 +2,48 @@
 var erpagWeather=angular.module('erpagWeather',['ngRoute','ngResource']);
 
 // app.js
-angular.module('erpagWeather', ['auth0', 'angular-storage', 'angular-jwt']);
-
-erpagWeather.config(function (authProvider) {
-  authProvider.init({
-    domain: 'myday.eu.auth0.com',
-    clientID: 'oFP2Wct8YTQvmWjmKAk4YgGmfLVZwJsl'
-  });
-})
-
-erpagWeather.run(function(auth) {
-  // This hooks al auth events to check everything as soon as the app starts
-  auth.hookEvents();
-});
-
-erpagWeather.config(function (authProvider, $routeProvider, $httpProvider, jwtInterceptorProvider) {
-  // ...
-
-  // We're annotating this function so that the `store` is injected correctly when this file is minified
-  jwtInterceptorProvider.tokenGetter = ['store', function(store) {
-    // Return the saved token
-    return store.get('token');
-  }];
-
-  $httpProvider.interceptors.push('jwtInterceptor');
-  // ...
-});
-
-erpagWeather.run(function($rootScope, auth, store, jwtHelper, $location) {
-  // This events gets triggered on refresh or URL change
-  $rootScope.$on('$locationChangeStart', function() {
-    var token = store.get('token');
-    if (token) {
-      if (!jwtHelper.isTokenExpired(token)) {
-        if (!auth.isAuthenticated) {
-          auth.authenticate(store.get('profile'), token);
-        }
-      } else {
-        // Either show the login page or use the refresh token to get a new idToken
-        $location.path('/index');
-      }
-    }
-  });
-});
+//angular.module('erpagWeather', ['auth0', 'angular-storage', 'angular-jwt']);
+//
+//erpagWeather.config(function (authProvider) {
+//
+//  authProvider.init({
+//    domain: 'myday.eu.auth0.com',
+//    clientID: 'oFP2Wct8YTQvmWjmKAk4YgGmfLVZwJsl'
+//  });
+//})
+//
+//erpagWeather.run(function(auth) {
+//  // This hooks al auth events to check everything as soon as the app starts
+//  auth.hookEvents();
+//});
+//
+//erpagWeather.config(function (authProvider, $routeProvider, $httpProvider, jwtInterceptorProvider) {
+//  // ...
+//
+//  // We're annotating this function so that the `store` is injected correctly when this file is minified
+//  jwtInterceptorProvider.tokenGetter = ['store', function(store) {
+//    // Return the saved token
+//    return store.get('token');
+//  }];
+//
+//  $httpProvider.interceptors.push('jwtInterceptor');
+//  // ...
+//});
+//
+//erpagWeather.run(function($rootScope, auth, store, jwtHelper, $location) {
+//  // This events gets triggered on refresh or URL change
+//  $rootScope.$on('$locationChangeStart', function() {
+//    var token = store.get('token');
+//    if (token) {
+//      if (!jwtHelper.isTokenExpired(token)) {
+//        if (!auth.isAuthenticated) {
+//          auth.authenticate(store.get('profile'), token);
+//        }
+//      } else {
+//        // Either show the login page or use the refresh token to get a new idToken
+//        $location.path('/index');
+//          console.log("error location changer")
+//      }
+//    }
+//  });
+//});
