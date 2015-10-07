489512b95d42478f54bd86ba2114535950031691
nexnexter <nexnexter@gmail.com>
2015-10-07 14:48:02 +0200

    in

--- public/js/app.js
+++ public/js/app.js
@@ -10,3 +10,33 @@ var erpagWeather=angular.module('erpagWeather',['ngRoute','ngResource','auth0','
   // This hooks al auth events to check everything as soon as the app starts
   auth.hookEvents();
 });
+
+erpagWeather.config(function (authProvider, $routeProvider, $httpProvider, jwtInterceptorProvider) {
+  // ...
+
+  // We're annotating this function so that the `store` is injected correctly when this file is minified
+  jwtInterceptorProvider.tokenGetter = ['store', function(store) {
+    // Return the saved token
+    return store.get('token');
+  }];
+
+  $httpProvider.interceptors.push('jwtInterceptor');
+  // ...
+});
+
+erpagWeather.run(function($rootScope, auth, store, jwtHelper, $location) {
+  // This events gets triggered on refresh or URL change
+  $rootScope.$on('$locationChangeStart', function() {
+    var token = store.get('token');
+    if (token) {
+      if (!jwtHelper.isTokenExpired(token)) {
+        if (!auth.isAuthenticated) {
+          auth.authenticate(store.get('profile'), token);
+        }
+      } else {
+        // Either show the login page or use the refresh token to get a new idToken
+        $location.path('/');
+      }
+    }
+  });
+});
