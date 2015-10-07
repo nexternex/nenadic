f7fd10b3a56ed60765451ee33a99b1f515721ca4
nexnexter <nexnexter@gmail.com>
2015-10-07 10:41:01 +0200

    in

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-
-var express = require('express');
-    routes = require('./routes');
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  errorHandler = require('error-handler'),
+  morgan = require('morgan'),
+  routes = require('./routes'),
+  api = require('./routes/api'),
+  http = require('http'),
+  path = require('path');
 var app = module.exports=express();
 
 app.set('port', (process.env.PORT || 5000));
 
 // views is directory for all template files
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.static(__dirname + '/public'));
-  app.use(app.router);
+//  app.set('views', __dirname + '/views');
+//  app.set('view engine', 'ejs');
+//  app.use(bodyParser());
+//  app.use(express.methodOverride());
+//  app.use(express.static(__dirname + '/public'));
+//  app.use(app.router);
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(morgan('dev'));
+app.use(bodyParser());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
 
 //GET ROUTES
 app.get('/', routes.index);
