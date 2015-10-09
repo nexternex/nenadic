3fc2ca56c9bb30d22260dd89aa97dbba215100ae
nexnexter <nexnexter@gmail.com>
2015-10-09 10:25:21 +0200

    s

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ var express = require('express'),
 
 var app = module.exports=express();
 var mongoose = require('mongoose');
-
 var uristring='mongodb://nexnexter:n1g22s581,@waffle.modulusmongo.net:27017/bOs4amos';
 
 mongoose.connect(uristring, function (err, res) {
@@ -21,80 +20,22 @@ mongoose.connect(uristring, function (err, res) {
       }
     });
 
-//mongoose.connect('mongodb://nexnexter:n1g22s581,@waffle.modulusmongo.net:27017/bOs4amos');
-
 app.set('port', (process.env.PORT || 5000));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//BACKEND ROUTES
+require('./routes/routes.js')(app)
 //GET ROUTES//
 app.get('/', routes.index);
 app.get('/partials/:name', routes.partials);
 app.get('*', routes.index)
-
-
-//DB models
-var Todo = mongoose.model('Todo', {
-        text : String
-    });
-
-
-//BACKEND ROUTES
-
-app.get('/api/todos', function(req, res) {
-
-        // use mongoose to get all todos in the database
-        Todo.find(function(err, todos) {
-
-            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-            if (err)
-                res.send(err)
-
-            res.json(todos); // return all todos in JSON format
-        });
-    });
-
- app.post('/api/todos', function(req, res) {
-
-        // create a todo, information comes from AJAX request from Angular
-        Todo.create({
-            text : req.body.text,
-            done : false
-        }, function(err, todo) {
-            if (err)
-                res.send(err);
-
-            // get and return all the todos after you create another
-            Todo.find(function(err, todos) {
-                if (err)
-                    res.send(err)
-                res.json(todos);
-            });
-        });
-
-    });
- // delete a todo
-    app.delete('/api/todos/:todo_id', function(req, res) {
-        Todo.remove({
-            _id : req.params.todo_id
-        }, function(err, todo) {
-            if (err)
-                res.send(err);
-
-            // get and return all the todos after you create another
-            Todo.find(function(err, todos) {
-                if (err)
-                    res.send(err)
-                res.json(todos);
-            });
-        });
-    });
-
 
 //start server
 app.listen(app.get('port'), function() {
