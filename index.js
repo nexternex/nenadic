3b2093f910f179ec637c82c7dbea4f213cfaf80c
nexnexter <nexnexter@gmail.com>
2015-10-14 10:02:39 +0200

    a

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ app.use(express.static(path.join(__dirname, 'public')));
             lastname : req.body.lastname,
             company : req.body.company,
             address : req.body.address,
-            size : "1"
+            size : req.body.id_addsize
 		}, function(err, list) {
 			if (err)
 				res.send(err);
