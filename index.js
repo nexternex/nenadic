fbcc3d78d40260df029e609bb2f1b905a577634b
nexnexter <nexnexter@gmail.com>
2015-10-14 14:06:00 +0200

    a

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 	});
 // create list and send back all lists after creation
 	app.post('/api/lists', function(req, res) {
-        console.log(req.body.name+":"+req.body.adsize);
+        console.log(req.body.name+":"+req.body.add_size);
 		// create a list, information comes from AJAX request from Angular
 		List.create({
 			name :req.body.name,
