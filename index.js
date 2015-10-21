a2d3ea232369600418d8f10baca7720a747149c5
nexnexter <nexnexter@gmail.com>
2015-10-21 13:55:14 +0200

    a

--- index.js
+++ index.js
@@ -16,36 +16,36 @@ var express = require('express'),
 
 
 
-//upload files
+upload files
 //var controller = require('./upload.controller');
-//var router = module.exports = express.Router();
-//var multer = require('multer');
-//var fs = require('fs');
+var router = module.exports = express.Router();
+var multer = require('multer');
+var fs = require('fs');
 
-//router.use(multer({
-//    dest: './public/uploads',
-//    changeDest: function(dest, req, res){
-//        dest += '/haha/';
-//        try{
-//            stat = fs.statSync(dest);
-//        }catch(err){
-//            fs.mkdirSync(dest);
-//        }
-//        return dest;
-//    },
-//    onFileUploadStart: function(file){
-//        console.log('starting');
-//    }
-//}));
+router.use(multer({
+    dest: './public/uploads',
+    changeDest: function(dest, req, res){
+        dest += '/haha/';
+        try{
+            stat = fs.statSync(dest);
+        }catch(err){
+            fs.mkdirSync(dest);
+        }
+        return dest;
+    },
+    onFileUploadStart: function(file){
+        console.log('starting');
+    }
+}));
 
-//
-//router.post('/', sendResponse);
-//function sendResponse(req, res){
-//res.send('ok');
-//};
 
-//
-//module.exports = router;
+router.post('/', sendResponse);
+function sendResponse(req, res){
+res.send('ok');
+};
+
+
+module.exports = router;
 
 
 
