78d864d2cb651cdca10efa3cd950ae8bd646cf58
nexternex <nexnexter@gmail.com>
2015-10-22 00:44:12 +0200

    a

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -169,17 +169,17 @@ erpagWeather.controller('list1Controller', ['$scope', '$http','API', function ($
     };
     
     
-    ///file upload controler///
-//    $scope.uploadLogo = function(logoFile) {
-//    console.log('before_API');
-//      API.uploadLogo(logoFile).success(function (uploadResponse) {
-//          // Handle response from server
-//        console.log(uploadResponse);
-//      }).error(function (error) {
-//        // Handle error from server
-//        console.log(error);
-//      });
-//    };
+//    /file upload controler///
+    $scope.uploadLogo = function(logoFile) {
+    console.log('before_API');
+      API.uploadLogo(logoFile).success(function (uploadResponse) {
+          // Handle response from server
+        console.log(uploadResponse);
+      }).error(function (error) {
+        // Handle error from server
+        console.log(error);
+      });
+    };
     
 
 }]);
