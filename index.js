3821c4ca88de231e602d17d5528e519ca771a727
nexnexter <nexnexter@gmail.com>
2015-10-22 12:44:34 +0200

    a

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //IMAGES//
-app.use(multer({ dest: './uploads/',
+app.use(multer({ dest: './tmp/',
     rename: function (fieldname, filename) {
         return filename+Date.now();
     },
