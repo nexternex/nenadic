41d70fbf47ddb12c61e3bf46b66f368ed18e79fa
nexternex <nexnexter@gmail.com>
2016-04-24 13:50:26 +0200

    a

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -300,16 +300,16 @@ erpagWeather.controller('mainController', ['$scope', '$http', function ($scope, 
             });
     };
     
-$scope.data = {
-    cb1: true,
-    cb4: true,
-    cb5: false
-  };
-  $scope.message = 'false';
-  $scope.onChange = function(cbState) {
-  	$scope.message = cbState;
-  };
-});
+    $scope.data = {
+        cb1: true,
+        cb4: true,
+        cb5: false
+      };
+      $scope.message = 'false';
+      $scope.onChange = function(cbState) {
+        $scope.message = cbState;
+      };
+
 }]);
 
 
