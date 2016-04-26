72607f6cb3798e1bddd701bafae7e9f8f388f4d0
nexternex <nexnexter@gmail.com>
2016-04-27 00:06:05 +0200

    a

--- public/js/app.js
+++ public/js/app.js
@@ -35,7 +35,7 @@ erpagWeather.config(function (authProvider, $routeProvider, $httpProvider, jwtIn
       'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
       'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> cla
                     })
-    .backgroundPalette('grey', {
+    .backgroundPalette('green', {
     'default': '400', // by default use shade 400 from the pink palette for primary intentions
       'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
       'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
