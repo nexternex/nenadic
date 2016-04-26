0898ae1edd84fcaef85b0c8b8cbcc29e5cc85fde
nexternex <nexnexter@gmail.com>
2016-04-26 23:51:59 +0200

    a

--- public/js/app.js
+++ public/js/app.js
@@ -29,7 +29,13 @@ erpagWeather.config(function (authProvider, $routeProvider, $httpProvider, jwtIn
 //tehemse
     
   $mdThemingProvider.theme('default')
-    .primaryPalette('purple')
+    .primaryPalette('purple',                    
+    'default': '400', // by default use shade 400 from the pink palette for primary intentions
+      'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+      'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+      'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> cla
+                               )
+    .backgroundPalette('blue')
     .accentPalette('green');
     
 });
