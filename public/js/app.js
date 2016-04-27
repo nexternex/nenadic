62654f6984c97ccabf47aed188a0c7ae91f214fb
nexternex <nexnexter@gmail.com>
2016-04-27 19:54:43 +0200

    a

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 //MODULE Angular
-var erpagWeather=angular.module('erpagWeather',['ngRoute','ngResource','ngAnimate','ngAria','ngMaterial','material.svgAssetsCache','auth0','angular-storage', 'angular-jwt']);
+var erpagWeather=angular.module('erpagWeather',['ngRoute','ngResource','ngAnimate','ngAria','ngMaterial','auth0','angular-storage', 'angular-jwt']);
 
 erpagWeather.run(function(auth) {
   // This hooks al auth events to check everything as soon as the app starts
