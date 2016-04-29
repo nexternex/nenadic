15b8d89dff555cd273bc663ec711034c61ded079
nexternex <nexnexter@gmail.com>
2016-04-29 21:06:16 +0200

    a

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ var erpagWeather=angular.module('erpagWeather',['ngRoute','ngResource','ngAnimat
 erpagWeather.run(function(auth) {
   // This hooks al auth events to check everything as soon as the app starts
   auth.hookEvents();
-    console.log('step0001-a');
+  console.log('step0001-a');
 });
  
 //config app
@@ -13,7 +13,7 @@ erpagWeather.config(function (authProvider, $routeProvider, $httpProvider, jwtIn
   authProvider.init({
     domain: 'myday.eu.auth0.com',
     clientID: 'oFP2Wct8YTQvmWjmKAk4YgGmfLVZwJsl',
-    loginUrl: '/login'
+    loginUrl: '/index'
   });
     
   // We're annotating this function so that the `store` is injected correctly when this file is minified
@@ -60,67 +60,4 @@ erpagWeather.run(function($rootScope, auth, store, jwtHelper, $location) {
   });
 });
 
-//google maps//
-erpagWeather.factory('GoogleMaps', function() {
-var placeSearch, autocomplete;
-var componentForm = {
-  street_number: 'short_name',
-  route: 'long_name',
-  locality: 'long_name',
-  administrative_area_level_1: 'short_name',
-  country: 'long_name',
-  postal_code: 'short_name'
-    };
-});
-
-erpagWeather.factory('InitAutocomplete',function initAutocomplete() {
-  // Create the autocomplete object, restricting the search to geographical
-  // location types.
-  autocomplete = new google.maps.places.Autocomplete(
-      /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
-      {types: ['geocode']});
-
-  // When the user selects an address from the dropdown, populate the address
-  // fields in the form.
-  autocomplete.addListener('place_changed', fillInAddress);
-});
-                     
-                     
-erpagWeather.factory('FillInAddress',function fillInAddress() {
-  // Get the place details from the autocomplete object.
-  var place = autocomplete.getPlace();
-
-  for (var component in componentForm) {
-    document.getElementById(component).value = '';
-    document.getElementById(component).disabled = false;
-  }
-
-  // Get each component of the address from the place details
-  // and fill the corresponding field on the form.
-  for (var i = 0; i < place.address_components.length; i++) {
-    var addressType = place.address_components[i].types[0];
-    if (componentForm[addressType]) {
-      var val = place.address_components[i][componentForm[addressType]];
-      document.getElementById(addressType).value = val;
-    }
-  }
-});
-
-// Bias the autocomplete object to the user's geographical location,
-// as supplied by the browser's 'navigator.geolocation' object.
-erpagWeather.factory('Geolocate',function geolocate() { {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var geolocation = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      var circle = new google.maps.Circle({
-        center: geolocation,
-        radius: position.coords.accuracy
-      });
-      autocomplete.setBounds(circle.getBounds());
-    });
-  }
-}
-}); 
+ 
