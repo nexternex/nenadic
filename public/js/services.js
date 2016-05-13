6f1dea21286b019dc1d8cbc7ca7ea064b3510089
nexternex <nexnexter@gmail.com>
2016-05-14 01:20:01 +0200

    a

--- public/js/services.js
+++ public/js/services.js
@@ -75,29 +75,16 @@ $httpProvider.interceptors.push(['$q', '$location', '$localStorage', function($q
 
 //factory koji vraca sve evente
 erpagWeather.factory('setEvent', function($http) {
-var events = [];   
+    
+return{
+ getAll:function(){ 
 
  $.getJSON("http://cors.io/spreadsheets.google.com/feeds/list/11YuCLGXJ_wOb4doQSgcxWuBNZfU9L-oSRo7RqmMNJ4k/od6/public/values?alt=json", function(data) {
   //first row "title" column
-  events=data.Deferred;
   console.log(data.feed.entry[0]['gsx$title']['$t']);
-});   
-
-    
-$http.get('/api/todos')
-    .success(function(data) {
-        events = data;
-        console.log('table controller' +data);
-        })
-        .error(function(data) {
-        console.log('Error: ' + data);
-        });
- 
-  return {
-    all: function() {
-      return events;
-    }
-  };
+            });   
+        };
+    };
 });
 
 
