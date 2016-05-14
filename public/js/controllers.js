c73eb90c3f53e6f4bc5b7874e340dda6a95ec2e9
nexternex <nexnexter@gmail.com>
2016-05-14 23:26:32 +0200

    s

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -524,7 +524,7 @@ erpagWeather.factory('setEvent',function($http) {
         getAll:function(){ 
          $.getJSON("https://spreadsheets.google.com/feeds/list/11YuCLGXJ_wOb4doQSgcxWuBNZfU9L-oSRo7RqmMNJ4k/od6/public/values?alt=json-in-script&callback=?", function(data) {
           //first row "title" column
-          console.log(data.feed.entry.[0]['gsx$title']['$t']);
+          console.log(data.feed.entry[0]['gsx$title']['$t']);
              return data.feed.entry;
             });    
         },
