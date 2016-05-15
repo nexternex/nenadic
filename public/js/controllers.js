a83f7b6045960339777f97f39f90a555d10e1af0
nexternex <nexnexter@gmail.com>
2016-05-15 10:58:47 +0200

    q

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -472,7 +472,7 @@ erpagWeather.controller('list1Controller', ['$scope','$http','setEvent1', functi
 //povuci sve iz google spreadsheet-a   ;
     $scope.fromFactory=setEvent1.getAll();
     
-    console.log(fromFactory);
+    console.log($scope.fromFactory);
 // when landing on the page, get all lists and show them
     $http.get('/api/lists')
         .success(function(data) {
