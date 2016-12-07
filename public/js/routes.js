31a02ca7a23d306fd8ab259487e83b384f0286a4
nexternex <nexnexter@gmail.com>
2016-12-07 20:13:07 +0100

    a

--- public/js/routes.js
+++ public/js/routes.js
@@ -8,60 +8,33 @@ erpagWeather.config(function ($routeProvider){
     templateUrl:'../pages/home.htm',
     controller:'list1Controller'
     })
-    
     .when('/Home',{
     templateUrl:'../pages/home.htm',
     controller:'list1Controller',
     })
-
-    .when('/Forecast', {
-    templateUrl:'../pages/forecast.htm',
-    controller:'forecastController',
-    requiresLogin: true
-    })
-    
     .when('/login', {
     templateUrl: '../pages/login.htm',
     controller: 'LoginCtrl'       
     })
-    
     .when('/list', {
     templateUrl: '../pages/list.htm',
-    controller: 'list1Controller'        
-    })
-     .when('/comp', {
-    templateUrl: '../pages/companies.htm',
-    controller: 'list1Controller'        
-    })
-    .when('/hero', {
-    templateUrl: '../pages/hero.htm',
     controller: 'list1Controller'        
     })
     .when('/register', {
     templateUrl: '../pages/register.htm',
     controller: 'list1Controller',
-    requiresLogin: true
-        
+    requiresLogin: true   
     })
     .when('/mypage', {
     templateUrl: '../pages/mypage.htm',
     controller: 'list1Controller',
-    requiresLogin: false
-        
+    requiresLogin: false  
     })
     .when('/timeline', {
     templateUrl: '../pages/timeline.htm',
     controller: 'list1Controller',
     requiresLogin: true
-        
     })
-      .when('/dialog', {
-    templateUrl: '../pages/timeline.htm',
-    controller: 'list1Controller',
-    requiresLogin: true
-        
-    })
-      // Logged in route
     .when('/login', {
     templateUrl: '../pages/login.htm',
     controller: 'mainController',
