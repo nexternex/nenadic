125b4d79b3a805d9c195bd69f76cf0427fed2b48
nexternex <nexnexter@gmail.com>
2016-12-10 14:10:28 +0100

    a

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -300,7 +300,7 @@ erpagWeather.controller('mainController', ['$scope', '$http','$mdDialog', '$mdMe
 $scope.showCreate = function(ev) {     
     $mdDialog.show({ 
       controller: 'mainController',
-      templateUrl: '../pages/dialog1.htm',
+//      templateUrl: '../pages/dialog1.htm',
       parent: angular.element(document.body),
       targetEvent: ev,
       ok:'Obrisi!',
