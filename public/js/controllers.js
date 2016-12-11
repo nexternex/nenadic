a66320b45e4bf6b1fdfa722eec09605b9a5ed8eb
nexternex <nexnexter@gmail.com>
2016-12-11 12:19:44 +0100

    a

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -278,6 +278,7 @@ erpagWeather.controller('mainController', ['$scope', '$http','$mdDialog', '$mdMe
                 $scope.todos = data;
                 $scope.formData = {}; // clear the form so our user is ready to enter another
                 console.log('create data completed returning:'+data);
+                $scope.hide();
             })
             .error(function(data) {
                 console.log('Error: ' + data);
@@ -330,7 +331,6 @@ $scope.showConfirm = function(id) {
     
      $scope.hide = function() {
       $mdDialog.hide();
-      $scopee.createTodo();
     };
 
     $scope.cancel = function() {
