9d2748f77f6e8ba92890272a23c663ab31a80bab
nexternex <nexnexter@gmail.com>
2016-12-15 00:29:25 +0100

    a

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -463,15 +463,14 @@ erpagWeather.controller('list1Controller', ['$scope','$http', function ($scope,$
 //PROFIL KORISNIKA//
 erpagWeather.controller('profileController', ['$scope','$http','auth','$q', function ($scope,$http,auth,$q) {
     $scope.formData = {};
-    $scope.profiles = 
-  {
-    "_id": "Crvena Zvezda",
-    "lastname": "Marakana",
-    "size": null,
-    "category": null,
-    "c_id": "auth0|560bc8e4356e91543abd8704",
-    "__v": 0
-  };
+//    $scope.profiles = {
+//    "_id": "Crvena Zvezda",
+//    "lastname": "Marakana",
+//    "size": null,
+//    "category": null,
+//    "c_id": "auth0|560bc8e4356e91543abd8704",
+//    "__v": 0
+//  };
 
     $scope.auth = auth;
     
