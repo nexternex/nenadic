3fdaac11e9bcdfb97c5c2438f57e7476e01fb26d
nexternex <nexnexter@gmail.com>
2016-12-17 19:44:11 +0100

    s

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 //MODULE Angular
-var erpagWeather=angular.module('erpagWeather',['ngRoute','ngResource','ngAnimate','ngAria','ngMaterial','auth0','angular-storage', 'angular-jwt','md.data.table','infinite-scroll', 'angularFileUpload']);
+var erpagWeather=angular.module('erpagWeather',['ngRoute','ngResource','ngAnimate','ngAria','ngMaterial','auth0','angular-storage', 'angular-jwt','md.data.table','infinite-scroll']);
 erpagWeather.run(function(auth) {
   // This hooks al auth events to check everything as soon as the app starts
   auth.hookEvents();
