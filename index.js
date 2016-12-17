ba55546415600cc8b4fac4cb7bb22d2ebf5f6093
nexternex <nexnexter@gmail.com>
2016-12-17 21:41:16 +0100

    s

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ var express = require('express'),
     path = require('path');
 
  ///
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '/uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
-  }
-})
+// var storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, '/uploads')
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.fieldname + '-' + Date.now())
+//   }
+// })
 
-var upload = multer({ storage: storage });
+// var upload = multer({ storage: storage });
 
 
 
