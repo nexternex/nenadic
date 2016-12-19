780afae0e264592c8e4f80d5179a5fbef608ddae
nexternex <nexnexter@gmail.com>
2016-12-19 12:15:53 +0100

    a

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.post('/api/photo',function(req,res){
         if(err) {
             return res.end("Error uploading file.");
         }
-        res.end(lists);
+        res.end('File uploaded');
     });
 });
 //update image in database MONGO
