bfa6ded88e45de2259e0e9a3c3035baa1533d27a
nexternex <nexnexter@gmail.com>
2016-12-27 17:59:49 +0100

    s

--- index.js
+++ index.js
@@ -60,6 +60,78 @@ app.use(express.static(path.join(__dirname, 'public')));
  
 //IMAGES//
 
+/*
+ * Import required packages.
+ * Packages should be installed with "npm install".
+ */
+const express = require('express');
+const aws = require('aws-sdk');
+
+/*
+ * Set-up and run the Express app.
+ */
+const app = express();
+app.set('views', './views');
+app.use(express.static('./public'));
+app.engine('html', require('ejs').renderFile);
+app.listen(process.env.PORT || 3000);
+
+/*
+ * Load the S3 information from the environment variables.
+ */
+const S3_BUCKET = process.env.S3_BUCKET;
+
+/*
+ * Respond to GET requests to /account.
+ * Upon request, render the 'account.html' web page in views/ directory.
+ */
+app.get('/account', (req, res) => res.render('account.html'));
+
+/*
+ * Respond to GET requests to /sign-s3.
+ * Upon request, return JSON containing the temporarily-signed S3 request and
+ * the anticipated URL of the image.
+ */
+app.get('/sign-s3', (req, res) => {
+  const s3 = new aws.S3();
+  const fileName = req.query['file-name'];
+  const fileType = req.query['file-type'];
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+    Expires: 60,
+    ContentType: fileType,
+    ACL: 'public-read'
+  };
+
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if(err){
+      console.log(err);
+      return res.end();
+    }
+    const returnData = {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+    };
+    res.write(JSON.stringify(returnData));
+    res.end();
+  });
+});
+
+/*
+ * Respond to POST requests to /submit_form.
+ * This function needs to be completed to handle the information in
+ * a way that suits your application.
+ */
+app.post('/save-details', (req, res) => {
+  // TODO: Read POSTed form data and do something useful
+});
+
+
+// image 2
+
+
+
 app.post('/api/photo',function(req,res){
     upload(req,res,function(err) {
         console.log("user_id:"+req.body.user_id);
