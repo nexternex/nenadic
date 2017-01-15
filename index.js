ceaadb6386b6696e981294ccd66229827e52db58
nexternex <nexnexter@gmail.com>
2017-01-15 14:39:08 +0100

    a

--- index.js
+++ index.js
@@ -229,7 +229,7 @@ app.post('/api/save-details/:user_id', (req, res,err) => {
 // UPDATE LIST 
 // ++ oglasi/lisitng and send back all lists after creation
 	app.post('/api/lists_update', function(req, res) {
-        console.log(req.body.formData.name+":"+req.body.size.singleSelect);
+        console.log(req.body.formData.name+":"+ req.body.formData.company);
 		// create a list, information comes from AJAX request from Angular
 		List.update({ 'c_id': req.body.user_id },
 			 { $set: { 
