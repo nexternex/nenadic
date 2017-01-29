a85e93354854535c7f3c4971cc27696339a8d505
nexternex <nexnexter@gmail.com>
2017-01-29 12:23:59 +0100

    s

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,13 @@
 //MODULE Angular
 var myDay=angular.module('myDay',['ngRoute','ngResource','ngAnimate','ngAria','ngMaterial','auth0','angular-storage', 'angular-jwt','md.data.table','infinite-scroll']);
+
 myDay.run(function(auth) {
   // This hooks al auth events to check everything as soon as the app starts
   auth.hookEvents();
   console.log('step0001-a');
- 
+  lock.show({
+    icon: 'https://nenadic.herokuapp.com/img/187.jpg'
+  });
 });
  
 //config app
