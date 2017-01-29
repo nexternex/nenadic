b6d7807b601e036988eeee13f7a6173812b4bf23
nexternex <nexnexter@gmail.com>
2017-01-29 13:06:20 +0100

    s

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -14,19 +14,11 @@ myDay.controller('LoginCtrl', ['$scope', '$http', 'auth', 'store', '$location',f
 $scope.auth = auth;
   $scope.login = function () {
     auth.signin({
-        authButtons: {
-            "testConnection": {
-                displayName: "Test Conn", 
-                primaryColor: "#b7b7b7", 
-                foregroundColor: "#000000", 
-                icon: "http://example.com/icon.png"
-            },
-            "testConnection2": {
-                primaryColor: "#000000", 
-                foregroundColor: "#ffffff", 
-            }
-        },
-        icon: 'https://nenadic.herokuapp.com/img/187.jpg',
+        theme: {
+            logo: 'https://nenadic.herokuapp.com/img/187.jpg',
+            primaryColor: 'green'
+        },  
+        // icon: 'https://nenadic.herokuapp.com/img/187.jpg',
         language: 'es'
     }, function (profile, token) {
       // Success callback
