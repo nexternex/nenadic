de3dea8784af76675420247b9f833baa3d0b7a4a
nexternex <nexnexter@gmail.com>
2017-02-03 00:12:16 +0100

    s

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -204,6 +204,7 @@ console.log("MenuCtrl");
                 if( tema=="sale") {
                     document.getElementById('MyDay_header').style.backgroundImage = "url('/img/001.jpg')";
                     $scope.filters=MyDay_filterSetSale;
+                    consloe.log($scope.filters);
                     }
                 else if(tema=="cake"){
                     document.getElementById('MyDay_header').style.backgroundImage = "url('/img/002.jpg')";
