a8d38b96602711c5a3a45e71cb1c0371ae2cbef9
nexternex <nexnexter@gmail.com>
2017-02-02 01:05:31 +0100

    s

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -174,33 +174,35 @@ myDay.controller('MenuCtrl', function() {
       this.availableDirections = ['up', 'down', 'left', 'right'];
       this.selectedDirection = 'up';
 
- 
-        $scope.changeImage=function(tema){
-                var ln_tema=tema;
-                console.log(ln_tema);
 
-                if( ln_tema=="sale") {
-                    document.getElementById('.cd-header').style.backgroundImage = "url('/img/001.jpg')";
-                    alert(ln_tema);
-                    }
-                else if(ln_tema=="cake"){
-                    document.getElementById('.cd-header').style.backgroundImage = "url('/img/002.jpg')";
-                    alert(ln_tema);
-                }
-                else if(ln_tema=="music"){
-                    document.getElementById('.cd-header').style.backgroundImage = "url('/img/003.jpg')";
-                    alert(ln_tema);
-                }
-                else if(ln_tema=="foto"){
-                    document.getElementById('.cd-header').style.backgroundImage = "url('/img/004.jpg')";
-                    alert(ln_tema);
-                }
-                else{
-                    console.log("nema parametra:f(temax)")
-                }
 
-            };
-    });
+    //     $scope.changeImage=function(tema){
+    //             var ln_tema=tema;
+    //             console.log(ln_tema);
+                
+
+    //             if( ln_tema=="sale") {
+    //                 document.getElementById('.cd-header').style.backgroundImage = "url('/img/001.jpg')";
+    //                 alert(ln_tema);
+    //                 }
+    //             else if(ln_tema=="cake"){
+    //                 document.getElementById('.cd-header').style.backgroundImage = "url('/img/002.jpg')";
+    //                 alert(ln_tema);
+    //             }
+    //             else if(ln_tema=="music"){
+    //                 document.getElementById('.cd-header').style.backgroundImage = "url('/img/003.jpg')";
+    //                 alert(ln_tema);
+    //             }
+    //             else if(ln_tema=="foto"){
+    //                 document.getElementById('.cd-header').style.backgroundImage = "url('/img/004.jpg')";
+    //                 alert(ln_tema);
+    //             }
+    //             else{
+    //                 console.log("nema parametra:f(temax)")
+    //             }
+
+    //         };
+    // });
 
     
 
