fc9d2765d20a50cd1683f598da0bd4ed495637b3
nexternex <nexnexter@gmail.com>
2017-02-05 21:15:37 +0100

    z

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -229,59 +229,6 @@ console.log("MenuCtrl");
 
 
 
-//gridList kontroler
-
- myDay.controller('gridListCtrl', function($scope) {
-
-    this.tiles = buildGridModel({
-            icon : "avatar:svg-",
-            title: "Svg-",
-            background: ""
-          });
-
-    function buildGridModel(tileTmpl){
-      var it, results = [ ];
-
-      for (var j=0; j<11; j++) {
-
-        it = angular.extend({},tileTmpl);
-        it.icon  = it.icon + (j+1);
-        it.title = it.title + (j+1);
-        it.span  = { row : 1, col : 1 };
-
-        switch(j+1) {
-          case 1:
-            it.background = "red";
-            it.span.row = it.span.col = 2;
-            break;
-
-          case 2: it.background = "green";         break;
-          case 3: it.background = "darkBlue";      break;
-          case 4:
-            it.background = "blue";
-            it.span.col = 2;
-            break;
-
-          case 5:
-            it.background = "yellow";
-            it.span.row = it.span.col = 2;
-            break;
-
-          case 6: it.background = "pink";          break;
-          case 7: it.background = "darkBlue";      break;
-          case 8: it.background = "purple";        break;
-          case 9: it.background = "deepBlue";      break;
-          case 10: it.background = "lightPurple";  break;
-          case 11: it.background = "yellow";       break;
-        }
-
-        results.push(it);
-      }
-      return results;
-    }
-  });
-    
-
 //Maps controller
 myDay.controller('MapsCtrl', ['$scope','GoogleMaps','InitAutocomplete','FillInAddress','Geolocate', function($scope,GoogleMaps,InitAutocomplete,FillInAddress,Geolocate) {
      console.log("maps kontroler entry");
@@ -309,8 +256,9 @@ myDay.controller('ListController', ['$scope','$http', function ($scope,$http) {
         })
         .error(function(data) {
             console.log('Error: ' + data);
-        });    
-    
+        });   
+
+        //INFINITE SCROLL
         // In this example, we set up our model using a plain object.
         // Using a class works too. All that matters is that we implement
         // getItemAtIndex and getLength.
