2561e06ce75359806c0a9df23e0b31d8ae8e0635
nexternex <nexnexter@gmail.com>
2017-02-09 21:07:06 +0100

    s

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -174,7 +174,8 @@ myDay.controller('MenuCtrl',['$scope', function($scope) {
                 ["sectionFeat",["bazen","terasa","otvoren"]]
             ];
         // filteri za muziku
-            var MyDay_filterSetMuzika=[ 
+            var MyDay_filterSetMuzika=[
+                 
                 ["Clanova Benda",[2,4,6]],
                 ["Vrsta Benda",["moderan","rockpop","tamburaski","starogradski"]],
                 ["Cena",[120,140,150,160,170,1800,1900,2000]],
@@ -249,47 +250,7 @@ myDay.controller('ListController', ['$scope','$http', function ($scope,$http) {
         })
         .error(function(data) {
             console.log('Error: ' + data);
-        });   
-
-        //INFINITE SCROLL
-        // In this example, we set up our model using a plain object.
-        // Using a class works too. All that matters is that we implement
-        // getItemAtIndex and getLength.
-        this.infiniteItems = {
-          numLoaded_: 0,
-          toLoad_: 0,
-
-          // Required.
-          getItemAtIndex: function(index) {
-            if (index > this.numLoaded_) {
-              this.fetchMoreItems_(index);
-              return null;
-            }
-
-            return index;
-          },
-
-          // Required.
-          // For infinite scroll behavior, we always return a slightly higher
-          // number than the previously loaded items.
-          getLength: function() {
-            return this.numLoaded_ + 5;
-          },
-
-          fetchMoreItems_: function(index) {
-            // For demo purposes, we simulate loading more items with a timed
-            // promise. In real code, this function would likely contain an
-            // $http request.
-
-            if (this.toLoad_ < index) {
-              this.toLoad_ += 20;
-              $timeout(angular.noop, 300).then(angular.bind(this, function() {
-                this.numLoaded_ = this.toLoad_;
-              }));
-            }
-          }
-        };
-
+        });  
 }]);
 
 //PROFIL KORISNIKA//
@@ -366,7 +327,7 @@ myDay.controller('ProfileController', ['$scope','$http','auth','$q','$timeout', 
         });
     }; 
 // UPDATE image url
- $scope.updateImageUrl = function() {
+    $scope.updateImageUrl = function() {
        $http.post('/api/save-details/'+ auth.profile.user_id)
         .success(function(data) {
             console.log('Update image url');
@@ -405,3 +366,80 @@ myDay.factory('todosService', function($http) {
   };
 });
 
+
+//KONTROLER: Infinite scroll - virtual scroll
+myDay.controller('virtualCtrl', function($timeout) {
+
+   var googleSpreadsheet= $http.jsonp('https://spreadsheets.google.com/feeds/list/11YuCLGXJ_wOb4doQSgcxWuBNZfU9L-oSRo7RqmMNJ4k/od6/public/values?alt=json-in-script&callback=JSON_CALLBACK')
+        .success(function(data) {
+            $scope.lists = data.feed.entry;
+            $scope.isLoading = false;
+//            console.log('liste sam dobio iz baze:'+data.feed.entry);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });  
+
+
+        // In this example, we set up our model using a class.
+        // Using a plain object works too. All that matters
+        // is that we implement getItemAtIndex and getLength.
+        var DynamicItems = function() {
+          /**
+           * @type {!Object<?Array>} Data pages, keyed by page number (0-index).
+           */
+          this.loadedPages = {};
+
+          /** @type {number} Total number of items. */
+          this.numItems = 0;
+
+          /** @const {number} Number of items to fetch per request. */
+          this.PAGE_SIZE = 50;
+
+          this.fetchNumItems_();
+        };
+
+        // Required.
+        DynamicItems.prototype.getItemAtIndex = function(index) {
+          var pageNumber = Math.floor(index / this.PAGE_SIZE);
+          var page = this.loadedPages[pageNumber];
+
+          if (page) {
+            return page[index % this.PAGE_SIZE];
+          } else if (page !== null) {
+            this.fetchPage_(pageNumber);
+          }
+        };
+
+        // Required.
+        DynamicItems.prototype.getLength = function() {
+          return this.numItems;
+        };
+
+        DynamicItems.prototype.fetchPage_ = function(pageNumber) {
+          // Set the page to null so we know it is already being fetched.
+          this.loadedPages[pageNumber] = null;
+
+          // For demo purposes, we simulate loading more items with a timed
+          // promise. In real code, this function would likely contain an
+          // $http request.
+          $timeout(angular.noop, 300).then(angular.bind(this, function() {
+            this.loadedPages[pageNumber] = [];
+            var pageOffset = pageNumber * this.PAGE_SIZE;
+            for (var i = pageOffset; i < pageOffset + this.PAGE_SIZE; i++) {
+              this.loadedPages[pageNumber].push(i);
+            }
+          }));
+        };
+
+        DynamicItems.prototype.fetchNumItems_ = function() {
+          // For demo purposes, we simulate loading the item count with a timed
+          // promise. In real code, this function would likely contain an
+          // $http request.
+          $timeout(angular.noop, 300).then(angular.bind(this, function() {
+            this.numItems = 50000;
+          }));
+        };
+        
+        this.dynamicItems = googleSpreadsheet();
+      });
