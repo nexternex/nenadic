21d86238c3c8057ba97f8206075c24f1934bb041
nexternex <nexnexter@gmail.com>
2017-02-23 23:16:07 +0100

    s

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -31,7 +31,7 @@
       // Success callback
       store.set('profile', profile);
       store.set('token', token);
-      $location.path('/mypage');
+      $location.path('/register');
     }, function () {
         console.log('error controller login');
         alert("please login");
