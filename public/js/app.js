c07fdc2a644ffd1fb88667ddc9a4057e46671e87
nexternex <nexnexter@gmail.com>
2017-02-26 11:29:49 +0100

    s

--- public/js/app.js
+++ public/js/app.js
@@ -40,12 +40,17 @@ myDay.config(function (authProvider, $routeProvider, $httpProvider, jwtIntercept
       'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> cla
                     })
     .backgroundPalette('grey', {
-    'default': '50', // by default use shade 400 from the pink palette for primary intentions
+    'default': '50', // by default use shade 400 from the gray palette for primary intentions
       'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
       'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
       'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> cla
                     })
-    .accentPalette('green');
+    .accentPalette('green',{
+      'default': '50', // by default use shade 400 from the green palette for primary intentions
+      'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+      'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+      'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> cla
+    });
 });
 
 myDay.run(function($rootScope, auth, store, jwtHelper, $location) {
