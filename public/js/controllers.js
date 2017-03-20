fdaf8465ad46e6741847c0350a35ba8d76c07e67
nexternex <nexnexter@gmail.com>
2017-03-20 23:31:49 +0100

    s

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -268,7 +268,7 @@
     }]);
 //KONTROLER: List controller--dropdown komponente
     myDay.controller('ListController', ['$scope','$http','$mdDialog', function ($scope,$http,$mdDialog,auth) {
-        $scope.filterBy = ['1'];
+        $scope.filterBy = ['Tik tak'];
         //modal detalji kompanije iz liste
         $scope.showAdvanced = function(e,item,auth) {
             $scope.auth=auth;
