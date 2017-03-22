23122b20b1b63f2a51f01f2201190a9a41528a4d
nexternex <nexnexter@gmail.com>
2017-03-22 21:03:22 +0100

    s

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -268,9 +268,9 @@
     }]);
 //KONTROLER: List controller--dropdown komponente
     myDay.controller('ListController', ['$scope','$http','$mdDialog', function ($scope,$http,$mdDialog,auth) {
-      $scope.filterValues = ["1","8"];
+      $scope.filterValues = [1,8];
       $scope.myFilter = function(value) {
-     return ($scope.filterValues.indexOf(value.gsx$naziv.$t) !== -1);
+     return ($scope.filterValues.indexOf(value.gsx$id.$t) !== -1);
 };
         //modal detalji kompanije iz liste
         $scope.showAdvanced = function(e,item,auth) {
