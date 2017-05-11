0a5bb89dd67d77bbd1b56e241d72953cfe74cbb1
nexternex <nexnexter@gmail.com>
2017-05-11 07:36:27 +0200

    s

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -288,7 +288,7 @@
                 if( tema=="sale") {
                     document.getElementById('MyDay_header').style.backgroundImage = "url('/img/001.jpg')";
                     $scope.filters=MyDay_filterSetSale;
-                    $scope.search="Sale";
+                    $scope.search.gsx$tip="Sale";
                     console.log("sale: "+$scope.filters);
 
                     }
