34ec52c3a32455ffbc6cf667001a38af4bba7465
nexternex <nexnexter@gmail.com>
2017-06-30 00:08:09 +0200

    s

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -327,6 +327,20 @@
                             vm.item = {};
                             vm.item = item;  //your object from the ng-repeat
 
+
+                              $scope.heart1=function(){
+                                    $http.get('/api/users'+auth.profile.user_id)
+                                        .success(function(data) {
+                                            //rezultat 1 profila ide u data
+                                    $rootScope.filterValues=data[0].ido;
+                                    console.log("filterValues2:"+$rootScope.filterValues);
+                                        })
+                                        .error(function(data) {
+                                            console.log('Error: ' + data);
+                                        });
+                                    };    
+                            $scope.heart1();
+                            
                             var likes = $rootScope.filterValues;
                             // var a = likes.indexOf(vm.item.gsx$id.$t);
 
