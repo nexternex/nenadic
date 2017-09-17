620d34e0baef0dc5f39e07f82625d18058fbc607
nexternex <nexnexter@gmail.com>
2017-09-17 13:54:41 +0200

    s

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -477,7 +477,7 @@
         // demo="2PACX-1vRb_J-jIVbkU-6S4Ou4gyXRv3muwVA2Yt7M4Ik_KJSs-t-E3UHIw0PGE1qXxhNr1k1nwgO2NyMwULAp"
         // production="11YuCLGXJ_wOb4doQSgcxWuBNZfU9L-oSRo7RqmMNJ4k/od6"
 
-        $http.jsonp('https://spreadsheets.google.com/feeds/list/2PACX-1vRb_J-jIVbkU-6S4Ou4gyXRv3muwVA2Yt7M4Ik_KJSs-t-E3UHIw0PGE1qXxhNr1k1nwgO2NyMwULAp/od6/public/values?alt=json-in-script&callback=JSON_CALLBACK')
+        $http.jsonp('https://spreadsheets.google.com/feeds/list/11YuCLGXJ_wOb4doQSgcxWuBNZfU9L-oSRo7RqmMNJ4k/od6/public/values?alt=json-in-script&callback=JSON_CALLBACK')
             .success(function(data) {
                 $scope.lists = data.feed.entry;
                 $scope.isLoading = false;
